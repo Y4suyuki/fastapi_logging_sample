69dcb93da8a4604cc69d77145b00482e1c3dea5b
Yasuyuki Ageishi <ageishi.yasuyuki@moneyforward.co.jp>
2022-08-17 11:58:05 +0900

    logging with basic config

--- fastapi/server/main.py
+++ fastapi/server/main.py
@@ -1,16 +1,27 @@
 from typing import Union
+import logging
 
 from fastapi import FastAPI
 
+from .helper.util import foo
+
+
 app = FastAPI()
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 @app.get("/")
 def read_root():
+    logger.warn("warn from root")
+    logger.info("info from root")
+    logger.debug("debug from root")
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
+    foo()
     return {"item_id": item_id, "q": q}
 
